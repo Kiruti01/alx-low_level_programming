350cf18fe1119c62735736fa017020addb675037
lord_kiruti <mikekirutic@gmail.com>
2023-07-06 13:29:30 +0300

    betty check

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * flipbits - counts number of bits to change from 1 to other
+ * flip_bits - counts num_bits to change from one num to other
  * @n: first num
- * @m: 2nd num
+ * @m: second num
  * Return: num of bits to change
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, count = 0;
 	unsigned long int current;
-	unsigned long int exclusv = n ^m;
+	unsigned long int exclus = n ^ m;
 
 	for (i = 63; i >= 0; i--)
 	{
-		current = exclusv >> i;
+		current = exclus >> i;
 		if (current & 1)
-			count i++;
+			count++;
 	}
 
 	return (count);
