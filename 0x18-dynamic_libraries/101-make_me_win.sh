df1e8a777ecafc1dfb0f0ec0fcbc5d47cbf19737
Kiruti01 <mikekirutic@gmail.com>
2023-08-21 22:06:12 +0300

    final task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Kiruti01/alx-low_level_programming/raw/master/0x18-dynamic_libraries/randomnum.so
-export LD_PRELOAD=/$PWD/randomnum.so
+wget -P /tmp https://github.com/Kiruti01/alx-low_level_programming/raw/main/0x18-dynamic_libraries/randomnum.so
+export LD_PRELOAD=/tmp/randomnum.so
+
+./gm 9 8 10 24 75 9
